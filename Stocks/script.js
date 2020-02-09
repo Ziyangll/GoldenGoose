f7800467bfc58e4a045a1afd54b7974bd82b07a8
Ziyang Li <zzl0106@auburn.edu>
2020-02-08 20:48:07 -0600

    0.1

---
+++ Stocks/script.js
@@ -0,0 +1,54 @@
+/**
+ * ---------------------------------------
+ * This demo was created using amCharts 4.
+ * 
+ * For more information visit:
+ * https://www.amcharts.com/
+ * 
+ * Documentation is available at:
+ * https://www.amcharts.com/docs/v4/
+ * ---------------------------------------
+ */
+
+// Themes begin
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+var chart = am4core.create("chartdiv", am4charts.XYChart);
+
+var data = [];
+var value = 50;
+for(var i = 0; i < 1000; i++){
+  var date = new Date();
+  date.setHours(0,0,0,0);
+  date.setDate(i);
+  value += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+  if (value < 20) {
+    value += Math.random() * 10;
+  }
+  data.push({date:date, value: value});
+}
+
+chart.data = data;
+
+// Create axes
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.minGridDistance = 60;
+
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+// Create series
+var series = chart.series.push(new am4charts.LineSeries());
+series.dataFields.valueY = "value";
+series.dataFields.dateX = "date";
+series.tooltipText = "{value}"
+
+series.tooltip.pointerOrientation = "vertical";
+
+chart.cursor = new am4charts.XYCursor();
+chart.cursor.snapToSeries = series;
+chart.cursor.xAxis = dateAxis;
+
+//chart.scrollbarY = new am4core.Scrollbar();
+chart.scrollbarX = new am4core.Scrollbar();
