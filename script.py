3b7018a298b9a77f103030685b4ef19e4bbd3427
Ziyang Li <zzl0106@auburn.edu>
2020-02-09 02:27:22 -0600

    links to other stocks

--- script.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-users = pymongo.MongoClient("mongodb+srv://DavidJoy:DHJ4clouddb@hackathonapp-6knru.gcp.mongodb.net/test?retryWrites=true&w=majority").test_database.users
-
-for x in users.find({"name":"test"}):
-	print(x)
-print("breaker")
-for x in users.find({"name":"no"}):
-	print(x)
